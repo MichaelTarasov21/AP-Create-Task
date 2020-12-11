db9733f4c0a9229ea912cb6c1d23d33824f29234
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2020-12-10 22:57:50 -0500

    Created foundational starting page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Hello");
+const Domselectors =  {
+    body: document.body
+}
+Domselectors.body.innerHTML = `<div id="starting_page"><h1>Guess the Number</h1><div id="intro_form"><p>Out of how much would you like to guess?</p><input class="num_input" id="maxnumber" type="number" min="2"><br><input type="checkbox" id="Limitoption"><label>Limit number of guesses</label><div id=guesslimitbox></div><input type="submit" value="Start the game" id="start_button"></div></div>`;
+console.log("You suck")
